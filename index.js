76a13ddf8466bab2de2136cf0ddf123df97f26a8
Touch Project <touch.project.inovailab@gmail.com>
2025-10-06 16:14:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,8 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-const uriRead = process.env.MONGODB_URI_READ;
-
-const uriWrite = process.env.MONGODB_URI_WRITE;
-
-const conn1 = mongoose.createConnection(uriRead, {useNewUrlParser: true});
-
-const conn2 = mongoose.createConnection(uriWrite, {useNewUrlParser: true});
+const conn1 = mongoose.createConnection(process.env.MONGODB_URI_READ, { useNewUrlParser: true, useUnifiedTopology: true });
+const conn2 = mongoose.createConnection(process.env.MONGODB_URI_WRITE, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const tactileSensationsSchema = {
    id_tactile: String,
